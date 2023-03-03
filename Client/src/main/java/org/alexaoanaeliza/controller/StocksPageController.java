cbe171640d70581ee6b577cee396822290f55838
alexaoanaeliza <oana_eliza_alexa@yahoo.com>
2023-03-03 23:14:07 +0200

    Beautify stocks table

--- Client/src/main/java/org/alexaoanaeliza/controller/StocksPageController.java
+++ Client/src/main/java/org/alexaoanaeliza/controller/StocksPageController.java
@@ -1,20 +1,26 @@
 package org.alexaoanaeliza.controller;
 
 import javafx.collections.FXCollections;
-import javafx.scene.control.TableColumn;
-import javafx.scene.control.TableView;
+import javafx.geometry.Insets;
+import javafx.geometry.Pos;
+import javafx.scene.control.*;
 import javafx.scene.control.cell.PropertyValueFactory;
+import javafx.scene.layout.Background;
+import javafx.scene.layout.BackgroundFill;
 import javafx.scene.layout.BorderPane;
+import javafx.scene.layout.CornerRadii;
+import javafx.scene.paint.Paint;
 import org.alexaoanaeliza.Stock;
 import org.alexaoanaeliza.User;
 import org.alexaoanaeliza.service.ServiceInterface;
 
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Objects;
 import java.util.Set;
 
 public class StocksPageController {
-    public TableView<Stock> stocksTableView;
-    public TableColumn<Stock, String> nameTableColumn;
-    public TableColumn<Stock, String> companyNameTableColumn;
+    public ListView<TextArea> stocksListView;
     private BorderPane mainBorderPane;
     private User user;
     public ServiceInterface service;
@@ -28,8 +34,15 @@ public class StocksPageController {
 
     private void setData() {
         Set<Stock> stocks = service.getStocks();
-        nameTableColumn.setCellValueFactory(new PropertyValueFactory<>("name"));
-        companyNameTableColumn.setCellValueFactory(new PropertyValueFactory<>("companyName"));
-        stocksTableView.setItems(FXCollections.observableArrayList(stocks));
+        List<TextArea> items = new ArrayList<>();
+        stocks.forEach(stock -> {
+            TextArea textArea = new TextArea(stock.getName() + "\n" + stock.getCompanyName());
+            textArea.setEditable(false);
+            textArea.setPrefHeight(50);
+            textArea.setPrefWidth(600);
+            textArea.setWrapText(true);
+            items.add(textArea);
+        });
+        stocksListView.setItems(FXCollections.observableArrayList(items));
     }
 }
