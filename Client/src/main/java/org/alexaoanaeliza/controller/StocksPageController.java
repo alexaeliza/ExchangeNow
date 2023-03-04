6dc0603bdf250d0b1b3c5f619ee225fec14ba7e4
alexaoanaeliza <oana_eliza_alexa@yahoo.com>
2023-03-04 14:33:12 +0200

    Adding service for calling python

--- Client/src/main/java/org/alexaoanaeliza/controller/StocksPageController.java
+++ Client/src/main/java/org/alexaoanaeliza/controller/StocksPageController.java
@@ -1,23 +1,17 @@
 package org.alexaoanaeliza.controller;
 
 import javafx.collections.FXCollections;
-import javafx.geometry.Insets;
-import javafx.geometry.Pos;
 import javafx.scene.control.*;
-import javafx.scene.control.cell.PropertyValueFactory;
-import javafx.scene.layout.Background;
-import javafx.scene.layout.BackgroundFill;
 import javafx.scene.layout.BorderPane;
-import javafx.scene.layout.CornerRadii;
-import javafx.scene.paint.Paint;
+import org.alexaoanaeliza.PredictionService;
 import org.alexaoanaeliza.Stock;
 import org.alexaoanaeliza.User;
 import org.alexaoanaeliza.service.ServiceInterface;
+import org.python.util.PythonInterpreter;
 
-import java.util.ArrayList;
-import java.util.List;
-import java.util.Objects;
-import java.util.Set;
+import java.io.*;
+import java.nio.charset.StandardCharsets;
+import java.util.*;
 
 public class StocksPageController {
     public ListView<TextArea> stocksListView;
@@ -35,10 +29,18 @@ public class StocksPageController {
     private void setData() {
         Set<Stock> stocks = service.getStocks();
         List<TextArea> items = new ArrayList<>();
+
+        try {
+            PredictionService predictionService = new PredictionService();
+            predictionService.read().forEach(System.out::println);
+        } catch (Exception e) {
+            System.out.println("Exception Raised" + e);
+        }
+
         stocks.forEach(stock -> {
             TextArea textArea = new TextArea(stock.getName() + "\n" + stock.getCompanyName());
             textArea.setEditable(false);
-            textArea.setPrefHeight(50);
+            textArea.setPrefHeight(70);
             textArea.setPrefWidth(600);
             textArea.setWrapText(true);
             items.add(textArea);
