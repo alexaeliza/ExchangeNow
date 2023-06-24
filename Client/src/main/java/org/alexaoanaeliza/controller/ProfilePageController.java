03827d60630e67653b81b88d30096188a8372304
alexaoanaeliza <oana_eliza_alexa@yahoo.com>
2023-06-24 14:34:02 +0300

    Add personal data to profile page

--- Client/src/main/java/org/alexaoanaeliza/controller/ProfilePageController.java
+++ Client/src/main/java/org/alexaoanaeliza/controller/ProfilePageController.java
@@ -1,7 +1,5 @@
 package org.alexaoanaeliza.controller;
 
-import javafx.collections.FXCollections;
-import javafx.collections.ObservableList;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.chart.PieChart;
@@ -9,17 +7,18 @@ import javafx.scene.control.Button;
 import javafx.scene.control.TextField;
 import javafx.scene.layout.BorderPane;
 import javafx.scene.layout.Pane;
-import org.alexaoanaeliza.Stock;
 import org.alexaoanaeliza.User;
 import org.alexaoanaeliza.service.ServiceInterface;
 
 import java.io.IOException;
-import java.util.Map;
 
 public class ProfilePageController {
-    public TextField portfolio;
-    public TextField investment;
-    public TextField returned;
+    public TextField name;
+    public TextField email;
+    public TextField invested;
+    public TextField available;
+    public TextField address;
+    public TextField personalNumber;
     public PieChart portfolioChart;
     public Button depositAmount;
     public Button withdrawAmount;
@@ -35,15 +34,12 @@ public class ProfilePageController {
     }
 
     private void setData() {
-        portfolio.setText("$" + service.getTodaySoldByUser(user).toString());
-        investment.setText("$" + user.getInvestedAmount().toString());
-        Double returnedValue = service.getReturnValueByUser(user);
-        if (returnedValue < 0D)
-            returned.setStyle("-fx-text-fill: red");
-        else
-            returned.setStyle("-fx-text-fill: green");
-        returned.setText("$" + returnedValue + " (" +
-                service.getReturnPercentageByUser(user) + "%)");
+        name.setText(user.getFirstName() + " " + user.getLastName());
+        email.setText(user.getEmail());
+        personalNumber.setText(user.getPersonalNumber());
+        address.setText(user.getCountry() + ", " + user.getCounty() + ", " + user.getCity() + ", " + user.getStreet() + ", " + user.getApartment());
+        available.setText(user.getAvailableAmount().toString());
+        invested.setText(user.getInvestedAmount().toString());
     }
 
     public void depositAmount(ActionEvent actionEvent) throws IOException {
